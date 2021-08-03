bcaf105c920bdcf91b1876f038cfee3cb00c3d32
aminesayagh <aminesayagh1997@gmail.com>
2021-08-03 22:08:42 +0100

    import

--- src/manga.js
+++ src/manga.js
@@ -1,4 +1,4 @@
-let slide = new Array();
-slide = document.querySelectorAll(".postContent img");
-console.log(slide);
-document.querySelector("#slideElement img").src = slide[0].dataset.src;
+// let slide = new Array();
+// slide = document.querySelectorAll(".postContent img");
+// document.querySelector("#slideElement img").src = slide[0].dataset.src;
+// console.log(slide);
