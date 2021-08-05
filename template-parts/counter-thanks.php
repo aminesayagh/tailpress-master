a802a2a52083f0aaeb68b10d7e0b6f3fc2bea557
aminesayagh <aminesayagh1997@gmail.com>
2021-08-05 12:33:09 +0100

    import

--- template-parts/counter-thanks.php
+++ template-parts/counter-thanks.php
@@ -1,11 +1,16 @@
 <div>
-      <button class="flex flex-row">
+      <button class="flex flex-row ">
             <p>
                   <?php
-                        echo get_field('thank', 'thanksCompter', true);
+                        $value = get_field('thank');
+                        if($value){
+                              echo $value;
+                        } else{
+                              echo 'nating';
+                        }
 		      ?>
             </p>
-            <div>
+            <div class="flex">
                   <svg version="1.1" style="width: 14px; fill: #FFF; margin-right: 5px;" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" xml:space="preserve"><path d="M24.85,10.126c2.018-4.783,6.628-8.125,11.99-8.125c7.223,0,12.425,6.179,13.079,13.543 c0,0,0.353,1.828-0.424,5.119c-1.058,4.482-3.545,8.464-6.898,11.503L24.85,48L7.402,32.165c-3.353-3.038-5.84-7.021-6.898-11.503 c-0.777-3.291-0.424-5.119-0.424-5.119C0.734,8.179,5.936,2,13.159,2C18.522,2,22.832,5.343,24.85,10.126z"></path></svg>
             </div>
             <p style="color: #fff">
