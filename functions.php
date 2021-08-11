d7e141b320e4989095409141158e3bd4c316bf89
aminesayagh <aminesayagh1997@gmail.com>
2021-08-11 14:35:07 +0100

    import myAnimation

--- functions.php
+++ functions.php
@@ -180,7 +180,7 @@ add_shortcode( 'my_button_counter', 'wpc_elementor_shortcode' );
 // wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
 function theme_gsap_script() {
     wp_enqueue_script( 'gsap-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js', array(), false, true );
-
+    wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . '/ressources/js/myAnimation.js', array(), false, true );
 //     wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . '/js/custom-gsap-scripts.js', array(), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
