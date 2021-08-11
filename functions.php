a5f46dbf3f656f8283bbe9a9a41fac47e2bcab95
aminesayagh <aminesayagh1997@gmail.com>
2021-08-11 13:44:43 +0100

    import animation cdn

--- functions.php
+++ functions.php
@@ -175,3 +175,12 @@ function wpc_elementor_shortcode() {
 };
 
 add_shortcode( 'my_button_counter', 'wpc_elementor_shortcode' );
+
+// The proper way to enqueue GSAP script
+// wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+function theme_gsap_script() {
+    wp_enqueue_script( 'gsap-js', 'http://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.1/TweenMax.min.js', array(), false, true );
+    
+//     wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . '/js/custom-gsap-scripts.js', array(), false, true );
+}
+add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
