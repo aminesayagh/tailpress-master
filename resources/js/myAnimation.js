bccd4798944b991397e812a781f13654b1fe98b6
aminesayagh <aminesayagh1997@gmail.com>
2021-08-14 17:27:50 +0100

    import

--- resources/js/myAnimation.js
+++ resources/js/myAnimation.js
@@ -2,6 +2,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.utils.toArray(".sectionElem").forEach((panel, i) => {
   ScrollTrigger.create({
+    xPercent: -100 * (sections.length - 1),
     trigger: panel,
     start: "top top",
     pin: true,
@@ -9,6 +10,6 @@ gsap.utils.toArray(".sectionElem").forEach((panel, i) => {
   });
 });
 
-// ScrollTrigger.create({
-//   snap: 1 / 2, // snap whole page to the closest section!
-// });
+ScrollTrigger.create({
+  snap: 1 / 2, // snap whole page to the closest section!
+});
