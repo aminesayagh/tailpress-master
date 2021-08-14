a8f02a50acdf012c6f543eac4cfe19cb8050faf3
aminesayagh <aminesayagh1997@gmail.com>
2021-08-14 14:48:03 +0100

    gi

--- resources/js/myAnimation.js
+++ resources/js/myAnimation.js
@@ -1,29 +1,36 @@
 ;(function () {
   // wait until gsap & ScrollTrigger available
-  let chck_if_gsap_loaded = setInterval(function () {
-    if (window.gsap && window.ScrollTrigger) {
-      // register scrolTrigger
-      gsap.registerPlugin(ScrollTrigger);
-      console.log("MY GSAP WORKING " + gsap);
-      const site_header = document.querySelector("#custom-header");
+  document.addEventListener("DOMContentLoaded", function(event) {
+    window.addEventListener("load", function (e) { 
+      
+      let chck_if_gsap_loaded = setInterval(function () {
+        if (window.gsap && window.ScrollTrigger) {
+          // register scrolTrigger
+          gsap.registerPlugin(ScrollTrigger);
+          console.log("MY GSAP WORKING " + gsap);
+          const site_header = document.querySelector("#custom-header");
+    
+          const show_hide_header = grap.from(site_header, {
+            // yPercent: -100,
+            // duration: 0.25,
+            // ease: "sine.out",
+          });
+    
+          ScrollTrigger.create({
+            start: "top top",
+            onUpdate: (self) => {
+              if (self.direction === -1) show_hide_header.play();
+              else show_hide_header.reverse();
+            },
+          });
+          // ... do your thing
+    
+          // clear interval
+          clearInterval(chck_if_gsap_loaded);
+        }
+      }, 500);
 
-      const show_hide_header = grap.from(site_header, {
-        // yPercent: -100,
-        // duration: 0.25,
-        // ease: "sine.out",
-      });
-
-      ScrollTrigger.create({
-        start: "top top",
-        onUpdate: (self) => {
-          if (self.direction === -1) show_hide_header.play();
-          else show_hide_header.reverse();
-        },
-      });
-      // ... do your thing
-
-      // clear interval
-      clearInterval(chck_if_gsap_loaded);
-    }
-  }, 500);
+      
+     }, false);
+  });
 })();
