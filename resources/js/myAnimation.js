2c44038b7df6160027f529da239e431cd02d2b59
aminesayagh <aminesayagh1997@gmail.com>
2021-08-14 16:59:13 +0100

    i

--- resources/js/myAnimation.js
+++ resources/js/myAnimation.js
@@ -1,24 +1,14 @@
-// wait until DOM is ready
-document.addEventListener("DOMContentLoaded", function (event) {
-  console.log("DOM loaded");
+gsap.registerPlugin(ScrollTrigger);
 
-  // wait until images, links, fonts, stylesheets, and js is loaded
-  window.addEventListener(
-    "load",
-    function (e) {
-      // custom GSAP code goes here
-      console.log("window loaded");
-      gsap.registerPlugin(ScrollTrigger);
+gsap.utils.toArray(".sectionElem").forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "top top",
+    pin: true,
+    pinSpacing: false,
+  });
+});
 
-      gsap.utils.toArray(".sectionElem").array.forEach((section) => {
-        ScrollTrigger.create({
-          trigger: section,
-          start: "top top",
-          pin: true,
-          pinSpacing: false,
-        });
-      });
-    },
-    false
-  );
+ScrollTrigger.create({
+  snap: 1 / 4, // snap whole page to the closest section!
 });
