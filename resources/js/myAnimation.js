bba2f7c51b958d3a9afea954dec1ed7c2f1985a8
aminesayagh <aminesayagh1997@gmail.com>
2021-08-14 19:10:15 +0100

    header

--- resources/js/myAnimation.js
+++ resources/js/myAnimation.js
@@ -10,31 +10,3 @@ gsap.utils.toArray(".sectionElem").forEach((panel, i) => {
   });
 });
 
-ScrollTrigger.create({
-  // snap: 1 / 26, // snap whole page to the closest section!
-});
-
-let header = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".elementor-location-header",
-    pin: true,
-    start: 'top top',
-    onUpdate: (self) => {
-      if (self.direction === -1 ) {
-        console.log('haut')
-        show_hide_header.play()
-      } else {
-        console.log('bas')
-        show_hide_header.reverse();
-      }
-    }
-  },
-});
-
-const show_hide_header = () => {
-  gsap.to(".elementor-location-header", {
-    yPercent: -100,
-    duration: 0.25,
-    ease: "sine.out"
-  });
-}
