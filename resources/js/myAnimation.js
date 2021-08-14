d1c668e9660a4ca02deb64848a28762570f5597c
aminesayagh <aminesayagh1997@gmail.com>
2021-08-14 15:47:24 +0100

    import

--- resources/js/myAnimation.js
+++ resources/js/myAnimation.js
@@ -1,39 +1,43 @@
 import { gsap } from "gsap";
 
-(function () {
-  // wait until gsap & ScrollTrigger available
-  let chck_if_gsap_loaded = setInterval(function () {
-    if (window.gsap && window.ScrollTrigger) {
-      // register scrolTrigger
+// (function () {
+//   // wait until gsap & ScrollTrigger available
+//   let chck_if_gsap_loaded = setInterval(function () {
+//     if (window.gsap && window.ScrollTrigger) {
+//       // register scrolTrigger
+//       gsap.registerPlugin(ScrollTrigger);
+//       console.log("MY GSAP WORKING " + gsap);
+//       const site_header = document.querySelector("#custom-header");
+
+//       const show_hide_header = grap.from(site_header, {
+//         yPercent: -100,
+//         duration: 0.25,
+//         ease: "sine.out",
+//       });
+
+//       ScrollTrigger.create({
+//         start: "top top",
+//         onUpdate: (self) => {
+//           if (self.direction === -1) show_hide_header.play();
+//           else show_hide_header.reverse();
+//         },
+//       });
+//       // ... do your thing
+
+//       // clear interval
+//       clearInterval(chck_if_gsap_loaded);
+//     }
+//   }, 500);
+
+// })();
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.log("DOM loaded");
+
+  window.addEventListener("load", (e) => {
+    console.log("window loaded")
+    if(window.gsap && window.ScrollTrigger) {
       gsap.registerPlugin(ScrollTrigger);
-      console.log("MY GSAP WORKING " + gsap);
-      const site_header = document.querySelector("#custom-header");
-
-      const show_hide_header = grap.from(site_header, {
-        yPercent: -100,
-        duration: 0.25,
-        ease: "sine.out",
-      });
-
-      ScrollTrigger.create({
-        start: "top top",
-        onUpdate: (self) => {
-          if (self.direction === -1) show_hide_header.play();
-          else show_hide_header.reverse();
-        },
-      });
-      // ... do your thing
-
-      // clear interval
-      clearInterval(chck_if_gsap_loaded);
     }
-  }, 500);
-
-  document.addEventListener("DOMContentLoaded", (e) => {
-    console.log("DOM loaded");
-
-    window.addEventListener("load", (e) => {
-      console.log("window loaded")
-    }, false);
-  });
-})();
+  }, false);
+});
