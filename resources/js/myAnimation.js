3f71305889efd2e678dd055f60b05acbb5a902d3
aminesayagh <aminesayagh1997@gmail.com>
2021-08-14 14:37:49 +0100

    gsap install

--- resources/js/myAnimation.js
+++ resources/js/myAnimation.js
@@ -1,39 +1,47 @@
-;( function() {
-
+(function () {
   // wait until gsap & ScrollTrigger available
-  let chck_if_gsap_loaded = setInterval( function() {
-    
-    if( window.gsap && window.ScrollTrigger ) {
-      
+  let chck_if_gsap_loaded = setInterval(function () {
+    if (window.gsap && window.ScrollTrigger) {
       // register scrolTrigger
-      gsap.registerPlugin( ScrollTrigger );
-      console.log('MY GSAP WORKING ' + gsap)
+      gsap.registerPlugin(ScrollTrigger);
+      console.log("MY GSAP WORKING " + gsap);
+      const site_header = document.querySelector("#custom-header");
+
+      const show_hide_header = grap.from(site_header, {
+        yPercent: -100,
+        duration: 0.25,
+        ease: "sine.out",
+      });
+
+      ScrollTrigger.create({
+        start: "top top",
+        onUpdate: (self) => {
+          if (self.direction === -1) show_hide_header.play();
+          else show_hide_header.reverse();
+        },
+      });
       // ... do your thing
-      show_hide_header();
 
       // clear interval
-      clearInterval( chck_if_gsap_loaded ); 
-      
+      clearInterval(chck_if_gsap_loaded);
     }
-    
-  }, 500 );
-  
+  }, 500);
+
   function show_hide_header() {
-		const site_header = document.querySelector( "#custom-header" );
-		
-		const show_hide_header = grap.from( site_header, {
-			yPercent: -100,
-			duration: 0.25,
-			ease: "sine.out",
-		});
-		
-		ScrollTrigger.create( {
-			start: "top top",
-			onUpdate: ( self ) => {
-				if(self.direction === -1) show_hide_header.play();
-				else show_hide_header.reverse();
-			}
-		} )
+    const site_header = document.querySelector("#custom-header");
+
+    const show_hide_header = grap.from(site_header, {
+      yPercent: -100,
+      duration: 0.25,
+      ease: "sine.out",
+    });
+
+    ScrollTrigger.create({
+      start: "top top",
+      onUpdate: (self) => {
+        if (self.direction === -1) show_hide_header.play();
+        else show_hide_header.reverse();
+      },
+    });
   }
-  
-} )();
+})();
