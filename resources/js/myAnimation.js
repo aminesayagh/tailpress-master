77dd74d4f683692741bd90e993c3dedd3980a351
aminesayagh <aminesayagh1997@gmail.com>
2021-08-14 17:33:54 +0100

    d

--- resources/js/myAnimation.js
+++ resources/js/myAnimation.js
@@ -2,12 +2,11 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.utils.toArray(".sectionElem").forEach((panel, i) => {
   ScrollTrigger.create({
-    xPercent: -100 * (sections.length - 1),
     trigger: panel,
     start: "top top",
     pin: true,
     pinSpacing: false,
-    // end= '+=600'
+    end= '+=600'
   });
 });
 
