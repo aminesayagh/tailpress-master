999158c04d9843bb3a6f725fc5a959a975f254ff
aminesayagh <aminesayagh1997@gmail.com>
2021-08-14 14:46:15 +0100

    gi

--- resources/js/myAnimation.js
+++ resources/js/myAnimation.js
@@ -1,4 +1,4 @@
-(function () {
+;(function () {
   // wait until gsap & ScrollTrigger available
   let chck_if_gsap_loaded = setInterval(function () {
     if (window.gsap && window.ScrollTrigger) {
@@ -7,7 +7,7 @@
       console.log("MY GSAP WORKING " + gsap);
       const site_header = document.querySelector("#custom-header");
 
-      const show_hide_header = grap.from("#custom-header", {
+      const show_hide_header = grap.from(site_header, {
         // yPercent: -100,
         // duration: 0.25,
         // ease: "sine.out",
@@ -26,22 +26,4 @@
       clearInterval(chck_if_gsap_loaded);
     }
   }, 500);
-
-  function show_hide_header() {
-    const site_header = document.querySelector("#custom-header");
-
-    const show_hide_header = grap.from(site_header, {
-      yPercent: -100,
-      duration: 0.25,
-      ease: "sine.out",
-    });
-
-    ScrollTrigger.create({
-      start: "top top",
-      onUpdate: (self) => {
-        if (self.direction === -1) show_hide_header.play();
-        else show_hide_header.reverse();
-      },
-    });
-  }
 })();
