55285dfdfa8b2ad3eac80a71ed7f36f264d8c24e
aminesayagh <aminesayagh1997@gmail.com>
2021-09-17 22:04:52 +0100

    import

--- template-parts/my_groupe_data_of_manga.php
+++ template-parts/my_groupe_data_of_manga.php
@@ -2,6 +2,12 @@
       <p>test</p>
       <?php
             $data = get_field('data_manga');
-            echo $data;
-      ?>
+            if($data) :
+            ?>
+                  <div class='container_data data_title'>
+                        <h4 class='title_data'>Format</h4>
+                        <h6 class='responce_data'><?php echo $data['format']?><h6>
+                  </div>
+                  
+      <?php endif; ?>      
 </div>
