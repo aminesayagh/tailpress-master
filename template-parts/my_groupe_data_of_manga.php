6eed71dc245eaea73f5600f0157ef5a7b42b444e
aminesayagh <aminesayagh1997@gmail.com>
2021-09-18 16:33:16 +0100

    IMPORT

--- template-parts/my_groupe_data_of_manga.php
+++ template-parts/my_groupe_data_of_manga.php
@@ -5,12 +5,17 @@
                   $content = "";
                   foreach($data as $key => $value){
                         if(is_array($value)){
-                              $content = $content . "<div class='container_data data_". $key."'><h4 class='title_data'>" . $key . "</h4><ul>";
-                              foreach($value as $subValue){
-                                    $content = $content . "<li>". $subValue . "</li>";
-                              }
-                        $content = $content . "</ul>";
-                        }else if($value != ''){
+                              if( have_rows($value)) :
+                                    $content = $content . "<div class='container_data data_". $key."'><h4 class='title_data'>" . $key . "</h4><ul>";
+                                    while(have_rows($value)) : the_row();
+                                          $sub_value = get_sub_field('sub_field');
+                                          $content = $content . "<li>". $sub_value . "</li>";
+                                          
+                                    endwhile;
+                                    $content = $content . "</ul>";
+                              else: 
+                              endif;
+                        } else if($value != ''){
                               $content = $content . "<div class='container_data data_". $key ."'><h4 class='title_data'>". $key ."</h4><h6 class='responce_data'>" . $value ."</h6></div>";
                         }
                   }
