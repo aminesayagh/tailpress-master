d18c6d109a6998affc6dcda558755ad11a39ab3d
aminesayagh <aminesayagh1997@gmail.com>
2021-09-18 16:09:28 +0100

    IMPORT

--- template-parts/my_groupe_data_of_manga.php
+++ template-parts/my_groupe_data_of_manga.php
@@ -4,7 +4,13 @@
             if($data) :
                   $content = "";
                   foreach($data as $key => $value){
-                        if($value != ''){
+                        if(is_array($value)){
+                              $content = $content . "<div class='container_data data_". $key."'><h4 class='title_data'>" . $key . "</h4><ul>";
+                              foreach($value as $subValue){
+                                    $content = $content . "<li>". $subValue . "</li>";
+                              }
+                        $content = $content . "</ul>";
+                        }else if($value != ''){
                               $content = $content . "<div class='container_data data_". $key ."'><h4 class='title_data'>". $key ."</h4><h6 class='responce_data'>" . $value ."</h6></div>";
                         }
                   }
