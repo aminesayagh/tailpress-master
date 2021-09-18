b4313a8283c585d527e19bd016ca83d3f40597c7
aminesayagh <aminesayagh1997@gmail.com>
2021-09-18 17:08:43 +0100

    import

--- template-parts/my_groupe_data_of_manga.php
+++ template-parts/my_groupe_data_of_manga.php
@@ -6,15 +6,7 @@
                   foreach($data as $key => $value){
                         if(is_array($value)){
                               $content = $content . "<div class='container_data data_". $key."'><h4 class='title_data'>" . $key . "</h4><ul>";
-                              if( have_rows($key)) :
-                                    while(have_rows($key)) : the_row();
-                                          $sub_value = get_sub_field('synonym');
-                                          $content = $content . "<li>". $sub_value . "</li>";
-                                          
-                                    endwhile;
-                                    $content = $content . "</ul>";
-                              else: 
-                              endif;
+                              
                         } else if($value != ''){
                               $content = $content . "<div class='container_data data_". $key ."'><h4 class='title_data'>". $key ."</h4><h6 class='responce_data'>" . $value ."</h6></div>";
                         }
@@ -23,3 +15,28 @@
             ?>
       <?php endif; ?>
 </div>
+
+<?php
+      $data = get_field('data_manga');
+      if( $data ) {
+            echo '<section class="data_manga_section">';
+            foreach( $data as $key => $value) {
+                  if($value !== '') {
+                        echo '<div class="container_data data"'. $key .'>';
+                        echo '<h4 class="title_data">'. $key .'</h4>';
+                        if(is_array($value)){
+                              echo '<ul class="data_repeater">';
+                              foreach($value as $sub_key => $sub_value){
+                                    echo '<li>';
+                                    echo $value[$sub_key];
+                                    echo '</li>';
+                              }
+                              echo '</ul>';
+                        } else if (is_string($value)){
+                              echo '<h6 class="responce_data">'. $value .'</h6>';
+                        }
+                        echo '</div>';
+                  }
+            }
+      }
+?>
