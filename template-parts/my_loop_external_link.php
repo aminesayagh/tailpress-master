d5a34c53e3ce6421a25edfb19badde891c2383ab
aminesayagh <aminesayagh1997@gmail.com>
2021-09-19 11:42:56 +0100

    import

--- template-parts/my_loop_external_link.php
+++ template-parts/my_loop_external_link.php
@@ -2,6 +2,7 @@
       $data = get_field('external_link');
       if( $data ) {
             echo '<section>';
+            echo '<h3>External & Streaming links</h3>';
             foreach( $data as $value){
                   echo '<div class="link">';
                   echo '<a href='. $value['link'] .'><div>'. $value['text'] .'</div></a>';
