4294cd7002c7b80bd0e88a95bd8f706297be5516
aminesayagh <aminesayagh1997@gmail.com>
2021-09-19 11:30:53 +0100

    import

--- functions.php
+++ functions.php
@@ -170,11 +170,11 @@ function tailpress_nav_menu_add_submenu_class( $classes, $args, $depth ) {
 
 add_filter( 'nav_menu_submenu_css_class', 'tailpress_nav_menu_add_submenu_class', 10, 3 );
 
-function wpc_elementor_shortcode_button_love() {
-	get_template_part( 'template-parts/counter-thanks', 'counter_thanks' );
-};
+// function wpc_elementor_shortcode_button_love() {
+// 	get_template_part( 'template-parts/counter-thanks', 'counter_thanks' );
+// };
 
-add_shortcode( 'my_button_counter', 'wpc_elementor_shortcode_button_love' );
+// add_shortcode( 'my_button_counter', 'wpc_elementor_shortcode_button_love' );
 
 // function wpc_elementor_shortcode_groupe_data_of_manga() {
 // 	get_template_part( 'template-parts/my_groupe_data_of_manga', 'my_groupe_data_of_manga');
