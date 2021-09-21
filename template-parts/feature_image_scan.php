99d87b4383f53c5581d898cef4c1cc2decdacf5c
aminesayagh <aminesayagh1997@gmail.com>
2021-09-21 20:46:25 +0100

    import

--- template-parts/feature_image_scan.php
+++ template-parts/feature_image_scan.php
@@ -4,10 +4,11 @@
 
       $terms = get_the_terms($id_scan, 'category');
 
-      echo $terms[0]->name;
+      
+      echo $terms[0];
       $relative_manga = get_page_by_title($terms[0]->name, 'OBJECT', 'manga');
 
-      var_dump($relative_manga);
+      // var_dump($relative_manga);
 
       $cover_manga = get_field('image_manga', $relative_manga->ID );
 
