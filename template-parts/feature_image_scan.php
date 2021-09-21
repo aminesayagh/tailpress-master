23305ce00fd8b407fedf2f6b0b53a280a9638b84
aminesayagh <aminesayagh1997@gmail.com>
2021-09-21 20:13:18 +0100

    import

--- template-parts/feature_image_scan.php
+++ template-parts/feature_image_scan.php
@@ -4,14 +4,10 @@
 
       $terms = get_the_terms($id_scan, 'category');
 
-      var_dump($terms[0]);
+      var_dump($terms);
 
-      $manga_relative = get_posts(array(
-            'post_type' => 'manga',
-            'post_title' => $terms,
-      ));
+      
 
-      var_dump($manga_relative);
 
 
 
