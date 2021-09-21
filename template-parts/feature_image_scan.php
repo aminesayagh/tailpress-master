e44788d4f45cf8a271cafe8c046cb78f17521fc9
aminesayagh <aminesayagh1997@gmail.com>
2021-09-21 20:58:16 +0100

    import

--- template-parts/feature_image_scan.php
+++ template-parts/feature_image_scan.php
@@ -13,9 +13,9 @@
       // var_dump($cover_manga);
 
       if( ! empty($cover_manga) ){
-
+            
             echo '<div class="cover_img">';
-            echo '<img src="'. ($cover_manga['url']) .' alt="'. esc_attr($relative_manga->name) .'-cover">';
+            echo '<img src="'. ($cover_manga) .' alt="'. esc_attr($relative_manga->name) .'-cover">';
             echo '</div>';
       }
 
