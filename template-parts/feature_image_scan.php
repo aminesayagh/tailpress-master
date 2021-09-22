0665cfb3ffb0f5246088f30686a476a536f11c3d
aminesayagh <aminesayagh1997@gmail.com>
2021-09-22 07:56:41 +0100

    import

--- template-parts/feature_image_scan.php
+++ template-parts/feature_image_scan.php
@@ -3,22 +3,22 @@
       $id_scan = get_the_ID();
       
       $post_type = get_post_type($id_scan);
-
-      if($post_type == 'manga'){
+      
+      $title_manga = get_the_title($id_scan);
+      
+      if($post_type == 'scan'){
             $terms = get_the_terms($id_scan, 'category');
-      
-            $relative_manga = get_page_by_title($terms[0]->name, 'OBJECT', 'manga');
-      
             
-            $cover_manga = get_field('image_manga', $relative_manga->ID );
-            // var_dump($cover_manga);
-      
-            if( ! empty($cover_manga) ){
-                  
-                  echo '<div class="cover_img">';
-                  echo '<img src="'. ($cover_manga) .'" alt="'. $relative_manga->name .'-cover">';
-                  echo '</div>';
-            }
+            $relative_manga = get_page_by_title($terms[0]->name, 'OBJECT', 'manga');
+            
+            $cover_manga = get_field('image_manga', $relative_manga->ID );            
+      } else if ($post_type == 'manga') {
+            $cover_manga = get_field('image_manga', $id_scan );
+      }
+      if( ! empty($cover_manga) ){
+            echo '<div class="cover_img">';
+            echo '<img src="'. ($cover_manga) .'" alt="'. $title_manga .'-cover">';
+            echo '</div>';
       }
 
 
