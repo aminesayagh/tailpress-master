13f86c76d1deb77ecbcc1d179675cb50cbb9515f
aminesayagh <aminesayagh1997@gmail.com>
2021-10-01 17:17:20 +0100

    import

--- template-parts/show_more_scan.php
+++ template-parts/show_more_scan.php
@@ -7,9 +7,25 @@
       
       if( $raking_scan ){
             echo '<div>';
-            echo '<select name="other_chapter" class="other_chapter">';
-            echo '<option default><a class="h-24" href="#">' . get_the_title($id_scan) . '</a></option>';
-            echo '</select>';
+            echo '<div name="other_chapter" class="other_chapter">';
+            echo '<h5><a class="h-24" href="#">' . get_the_title($id_scan) . '</a></h5>';
+            echo '</div>';
+            echo '<div>';
+            
+            $args = array('post_type' => 'scan', 'category_name' => $category_scan, 'posts_per_page' => 20);
+            
+            $my_query = new WP_Query( $args );
+            
+            if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
+                  echo '<h5>';
+                  the_title();
+                  echo '</h5>';
+            endwhile;
+            endif;
+
+            wp_reset_postdata();
+            
+            
             echo '</div>';
       }
 ?>
