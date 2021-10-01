f6bb95413fba0b03eb044fe9d5ab33a6bcda6818
aminesayagh <aminesayagh1997@gmail.com>
2021-10-01 20:07:12 +0100

    import

--- template-parts/show_more_scan.php
+++ template-parts/show_more_scan.php
@@ -6,11 +6,12 @@
       $link_category_scan = 'https://shinobyboy.co/manga/'. $category_scan;
       
       if( $raking_scan ){
-            echo '<div>';
-            echo '<div name="other_chapter" class="other_chapter">';
-            echo '<h5><a class="h-24" href="#">' . get_the_title($id_scan) . '</a></h5>';
+            echo '<div class="relative">';
+            
+            echo '<div name="other_chapter" class="title_selector_more_chapter">';
+            echo '<h5 class="main_title">' . get_the_title($id_scan) . '</h5>';
             echo '</div>';
-            echo '<div>';
+            
             
             $args = array(
                   'post_type' => 'scan', 
@@ -22,9 +23,9 @@
             );
             
             $my_query = new WP_Query( $args );
-            
+            echo '<div class="absolute hidden h-0 content_selector">';
             if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
-                  echo '<h5 style="color: #fff">';
+                  echo '<h5 style="color: #fff" class="elem_selector_more_chapter">';
                   echo '<a href=';
                   echo the_permalink();
                   echo '>';
@@ -33,7 +34,10 @@
                   echo '</h5>';
             endwhile;
             endif;
-
+            echo '</div>';
+            
+            echo '</div>';
+            
             wp_reset_postdata();
             
             
