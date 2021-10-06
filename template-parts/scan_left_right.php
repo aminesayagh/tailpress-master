af1588eec1b597d35d6822154e5eaa09d21ccb77
aminesayagh <aminesayagh1997@gmail.com>
2021-10-06 12:18:56 +0100

    import

--- template-parts/scan_left_right.php
+++ template-parts/scan_left_right.php
@@ -4,13 +4,31 @@
       $category_scan = get_the_category($id_scan);
       $raking = get_field('raking', $id_scan);
 
-      $my_query_left = new WP_Query(array(
+      $args = array(
             'post_type' => 'scan',
             'category_name' => $category_scan[0]->name,
             'posts_per_page' => 3,
-            'meta_key' => 'raking',
-            'meta_value' => array($raking - 1, $raking + 1),
-            'meta_type' => 'numeric',
-            'meta_compare' => 'BETWEEN',
-      ))
+            'meta_query' => array(
+                  'relation' => 'AND',
+                  array(
+                        'key' => 'raking',
+                        'value' => array ( $raking - 1, $raking + 1 ),
+                        'type' => 'numeric',
+                        'compare' => 'BETWEEN'
+                  ),
+                  array(
+                        'key' => 'raking',
+                        'value' => $raking,
+                        'type' => 'numeric',
+                        'compare' => '!=',
+                  )
+            )
+      );
+
+      $my_query = new WP_Query($args);
+
+      if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
+            echo '<a href=' . the_permalink() . '>'. the_title() .'</a>';
+      endwhile;
+      endif;
 ?>
