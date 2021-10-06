45940550726778af6c14ed45137b6f1983d92a2f
aminesayagh <aminesayagh1997@gmail.com>
2021-10-06 11:51:42 +0100

    import

---
+++ template-parts/scan_left_right.php
@@ -0,0 +1,17 @@
+<?php
+      $id_scan = get_the_ID();
+
+      $category_scan = get_the_category($id_scan);
+      $raking = get_field('raking', $id_scan);
+
+      $my_query_left = new WP_Query(array(
+            'post_type' => 'scan',
+            'category_name' => $category_scan[0]->name,
+            'posts_per_page' => 3,
+            'meta_key' => 'raking',
+            'meta_value' => array($raking - 1, $raking + 1),
+            'type' => 'numeric',
+            'compare' => 'BETWEEN',
+
+      ))
+?>
