702f700c95069c353b25d17ea9a4fc5899c6cf69
aminesayagh <aminesayagh1997@gmail.com>
2021-10-06 14:00:17 +0100

    import

--- template-parts/raking_num.php
+++ template-parts/raking_num.php
@@ -4,10 +4,15 @@
       $raking_scan = get_field('raking', $id_scan);
 
       if($raking_scan){
-            echo '<span class="flex flex-row items-center space-x-0">';
+            echo '<span class="flex flex-row items-center space-x-0 raking_scan">';
             echo '<h6>#<h6><h5>';
             echo $raking_scan + 1;
             echo '</h5>';
             echo '</span>';
       }
 ?>
+<style>
+      .raking_scan h5, .raking_scan h6{
+            color: #000000 !important;
+      }
+</style>
