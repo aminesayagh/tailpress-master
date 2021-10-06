cc44975bac833af65dd119643f6f72db62bee6f2
aminesayagh <aminesayagh1997@gmail.com>
2021-10-06 14:24:32 +0100

    import

--- template-parts/raking_num.php
+++ template-parts/raking_num.php
@@ -6,10 +6,14 @@
       if($raking_scan){
             echo '<span class="flex flex-row items-center space-x-0 raking_scan">';
             echo '<h6>#<h6><h5>';
-            if($raking_scan > 1000 ) {
+            if($raking_scan >= 1000 ) {
                   echo $raking_scan + 1;
-            } else if($raking_scan > 100 && $raking_scan < 1000) {
+            } else if($raking_scan >= 100 && $raking_scan < 1000) {
                   echo '0' .( $raking_scan + 1);
+            } else if($raking_scan >= 10 && $raking_scna < 100) {
+                  echo '00' .( $raking_scan + 1);
+            } else if($raking_scan < 10){
+                  echo '000' .( $raking_scan + 1);
             }
             echo '</h5>';
             echo '</span>';
