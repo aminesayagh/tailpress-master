1b4331427008fdad916ea44735bd91c16c9c5f94
aminesayagh <aminesayagh1997@gmail.com>
2021-10-10 12:56:59 +0100

    import

--- functions.php
+++ functions.php
@@ -227,12 +227,7 @@ add_action('elementor/query/query_scan_filter_by_title_manga', function( $query 
 	} else {
 		$post_eng_title = null;
 	}
-	if($post_title){
-		$query->set('category_name', $post_title);
-	}
-	if(!empty($post_eng_title)){
-		$query->set('category_name',$post_eng_title);
-	}
+	$query->set('category_name',[$post_eng_title, $post_title] );
 	$query->set('orderby', 'meta_value_num');
 	$query->set('meta_key', 'raking');
 	$query->set('order', 'DESC');
