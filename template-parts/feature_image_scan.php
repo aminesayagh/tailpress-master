7d5e9e5791a0e63e8917db95eba8f9752b1164e6
aminesayagh <aminesayagh1997@gmail.com>
2021-10-13 18:48:02 +0100

    import

--- template-parts/feature_image_scan.php
+++ template-parts/feature_image_scan.php
@@ -5,10 +5,6 @@
       
       $title_manga = get_the_title($id_scan);
       
-      echo var_dump($id_scan);
-      echo var_dump($post_type);
-      echo var_dump($title_manga);
-      
       if($post_type == 'scan'){
             $terms = get_the_terms($id_scan, 'category');
             
@@ -18,7 +14,6 @@
       } else if ($post_type == 'manga') {
             $cover_manga = get_field('image_manga', $id_scan );
       }
-      echo $cover_manga;
       if( ! empty($cover_manga) ){
             
             echo '<div class="cover_img">';
