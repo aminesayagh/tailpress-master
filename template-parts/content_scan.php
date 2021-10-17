30c898bb6ce9b1b8c7fb13a0bd87825fe517f1cc
aminesayagh <aminesayagh1997@gmail.com>
2021-10-17 17:38:54 +0100

    import

--- template-parts/content_scan.php
+++ template-parts/content_scan.php
@@ -1,7 +1,7 @@
 <?php
       $id_scan = get_the_ID();
       $post = get_post($id_scan);
-$content = apply_filters( 'the_content', $post );
+$content = apply_filters( 'the_content', $post->post_content );
 
 echo $content;
 ?>
