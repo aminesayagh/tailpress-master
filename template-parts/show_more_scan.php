724358beea948939a99d2e79fd562915336d9223
aminesayagh <aminesayagh1997@gmail.com>
2021-11-15 11:39:10 +0100

    import

--- template-parts/show_more_scan.php
+++ template-parts/show_more_scan.php
@@ -27,12 +27,6 @@
             $my_query = new WP_Query( $args );
             wp_reset_postdata();
 
-            $list_scan = array_column( $my_query->posts, 'raking' );
-            echo '<pre>';
-            echo var_dump($list_scan);
-            echo '</pre>';
-
-            
             echo '<div class="absolute z-20 hidden h-0 content_selector">';
             if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
                   echo '<a class="link_selector_more_chapter" href=';
@@ -45,7 +39,7 @@
             endwhile;
             endif;
             echo '</div>';
-            echo '<div class="fixed hidden screen_display"></div>';
+            echo '<div class="fixed z-10 hidden screen_display"></div>';
             echo '</div>';
             
             wp_reset_query();
