e0cb46fe18c24601bd2ff1d5c04786d441c2aa83
aminesayagh <aminesayagh1997@gmail.com>
2021-11-25 11:50:47 +0100

    import

--- template-parts/test_query_favorite.php
+++ template-parts/test_query_favorite.php
@@ -1,7 +1,7 @@
 <?php
 	$favorite = array($_COOKIE['my_favorites_mangas']);
       $args = array('post_type' => 'mangas', 'post__in' => $favorite);
-
+      echo $favorite;
       $my_query = new WP_Query($args);
       if( $my_query->have_posts() ) {while( $my_query->have_posts() ) : $my_query->the_post();
             echo the_permalink();
