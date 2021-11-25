294ffda9e1099ed5623845eb0cb450877a7ce3fa
aminesayagh <aminesayagh1997@gmail.com>
2021-11-25 11:50:00 +0100

    import

--- template-parts/test_query_favorite.php
+++ template-parts/test_query_favorite.php
@@ -1,4 +1,13 @@
 <?php
-	echo $_COOKIE['my_favorites_mangas'];
+	$favorite = array($_COOKIE['my_favorites_mangas']);
+      $args = array('post_type' => 'mangas', 'post__in' => $favorite);
 
+      $my_query = new WP_Query($args);
+      if( $my_query->have_posts() ) {while( $my_query->have_posts() ) : $my_query->the_post();
+            echo the_permalink();
+            echo the_title();
+      endwhile;
+      } else {
+      }
+      
 ?>
