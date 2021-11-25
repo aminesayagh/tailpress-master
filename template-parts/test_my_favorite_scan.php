9b13deb2a239092aff49176afc878138d5e68d5a
aminesayagh <aminesayagh1997@gmail.com>
2021-11-25 20:52:58 +0100

    import

--- template-parts/test_my_favorite_scan.php
+++ template-parts/test_my_favorite_scan.php
@@ -5,5 +5,17 @@
 		$title_of_manga = get_the_title($manga);
 		array_push($mangas_titles, $title_of_manga);
 	}
-      echo var_dump($mangas_titles);
+      $args = array(
+            'post_type' => 'scan',
+            'category__in' => $title_of_manga
+      );
+
+      $my_query = new WP_Query($args);
+
+      if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
+            echo the_title();
+            endwhile;
+            endif;
+            wp_reset_query();
+
 ?>
